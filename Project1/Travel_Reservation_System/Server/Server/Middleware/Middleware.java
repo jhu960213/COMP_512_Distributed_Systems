f2d0636a582274270bc072b6dd1296ce37de3365
haaaamza <mian.hamza@mail.mcgill.ca>
2021-09-23 16:34:11 -0400

    Added an Addcustomer, and DeleteCustomer Functionality.
    Both of us are working on the same branch now, might merge some better functionality from my branch here later, during debugging, as required.

--- Project1/Travel_Reservation_System/Server/Server/Middleware/Middleware.java
+++ Project1/Travel_Reservation_System/Server/Server/Middleware/Middleware.java
@@ -19,6 +19,7 @@ public class Middleware implements IResourceManager {
 
     protected String middlewareName;
     private HashMap<String, IResourceManager> resources;
+    protected RMHashMap m_data = new RMHashMap();
 
     public Middleware(String name) {
         try {
@@ -28,6 +29,128 @@ public class Middleware implements IResourceManager {
             System.out.println("\n*** Middleware error: " + e.getMessage() + " ***\n");
         }
     }
+
+  // Reads a data item
+  protected RMItem readData(int xid, String key)
+  {
+    synchronized(m_data) {
+      RMItem item = m_data.get(key);
+      if (item != null) {
+        return (RMItem)item.clone();
+      }
+      return null;
+    }
+  }
+
+  // Writes a data item
+  protected void writeData(int xid, String key, RMItem value)
+  {
+    synchronized(m_data) {
+      m_data.put(key, value);
+    }
+  }
+
+  // Remove the item out of storage
+  protected void removeData(int xid, String key)
+  {
+    synchronized(m_data) {
+      m_data.remove(key);
+    }
+  }
+
+  // Deletes the encar item
+  protected boolean deleteItem(int xid, String key)
+  {
+    Trace.info("RM::deleteItem(" + xid + ", " + key + ") called");
+    ReservableItem curObj = (ReservableItem)readData(xid, key);
+    // Check if there is such an item in the storage
+    if (curObj == null)
+    {
+      Trace.warn("RM::deleteItem(" + xid + ", " + key + ") failed--item doesn't exist");
+      return false;
+    }
+    else
+    {
+      if (curObj.getReserved() == 0)
+      {
+        removeData(xid, curObj.getKey());
+        Trace.info("RM::deleteItem(" + xid + ", " + key + ") item deleted");
+        return true;
+      }
+      else
+      {
+        Trace.info("RM::deleteItem(" + xid + ", " + key + ") item can't be deleted because some customers have reserved it");
+        return false;
+      }
+    }
+  }
+
+  // Query the number of available seats/rooms/cars
+  protected int queryNum(int xid, String key)
+  {
+    Trace.info("RM::queryNum(" + xid + ", " + key + ") called");
+    ReservableItem curObj = (ReservableItem)readData(xid, key);
+    int value = 0;
+    if (curObj != null)
+    {
+      value = curObj.getCount();
+    }
+    Trace.info("RM::queryNum(" + xid + ", " + key + ") returns count=" + value);
+    return value;
+  }
+
+  // Query the price of an item
+  protected int queryPrice(int xid, String key)
+  {
+    Trace.info("RM::queryPrice(" + xid + ", " + key + ") called");
+    ReservableItem curObj = (ReservableItem)readData(xid, key);
+    int value = 0;
+    if (curObj != null)
+    {
+      value = curObj.getPrice();
+    }
+    Trace.info("RM::queryPrice(" + xid + ", " + key + ") returns cost=$" + value);
+    return value;
+  }
+
+  // Reserve an item
+  protected boolean reserveItem(int xid, int customerID, String key, String location)
+  {
+    Trace.info("RM::reserveItem(" + xid + ", customer=" + customerID + ", " + key + ", " + location + ") called" );
+    // Read customer object if it exists (and read lock it)
+    Customer customer = (Customer)readData(xid, Customer.getKey(customerID));
+    if (customer == null)
+    {
+      Trace.warn("RM::reserveItem(" + xid + ", " + customerID + ", " + key + ", " + location + ")  failed--customer doesn't exist");
+      return false;
+    }
+
+    // Check if the item is available
+    ReservableItem item = (ReservableItem)readData(xid, key);
+    if (item == null)
+    {
+      Trace.warn("RM::reserveItem(" + xid + ", " + customerID + ", " + key + ", " + location + ") failed--item doesn't exist");
+      return false;
+    }
+    else if (item.getCount() == 0)
+    {
+      Trace.warn("RM::reserveItem(" + xid + ", " + customerID + ", " + key + ", " + location + ") failed--No more items");
+      return false;
+    }
+    else
+    {
+      customer.reserve(key, location, item.getPrice());
+      writeData(xid, customer.getKey(), customer);
+
+      // Decrease the number of available items in the storage
+      item.setCount(item.getCount() - 1);
+      item.setReserved(item.getReserved() + 1);
+      writeData(xid, item.getKey(), item);
+
+      Trace.info("RM::reserveItem(" + xid + ", " + customerID + ", " + key + ", " + location + ") succeeded");
+      return true;
+    }
+  }
 
     public HashMap<String, IResourceManager> getResources() {
         return resources;
@@ -230,19 +353,79 @@ public class Middleware implements IResourceManager {
     // TODO: need to ask prof how we want to add new customers to each resource servers
     public int newCustomer(int xid) throws RemoteException
     {
-        return 0;
+      Trace.info("RM::newCustomer(" + xid + ") called");
+      // Generate a globally unique ID for the new customer
+      int cid = Integer.parseInt(String.valueOf(xid) +
+        String.valueOf(Calendar.getInstance().get(Calendar.MILLISECOND)) +
+        String.valueOf(Math.round(Math.random() * 100 + 1)));
+      Customer customer = new Customer(cid);
+      writeData(xid, customer.getKey(), customer);
+      Trace.info("RM::newCustomer(" + cid + ") returns ID=" + cid);
+      //we call the second newcustomer function, in order to ensure the same CustomerID among all servers.
+      //we don't use the same newcustomer(xid) function in the other servers, in order to maintain synchronization.
+      this.getResources().get("roomServer").newCustomer(xid, cid);
+      this.getResources().get("carServer").newCustomer(xid, cid);
+      this.getResources().get("flightServer").newCustomer(xid, cid);
+      return cid;
     }
 
     // TODO: need to ask prof how we want to add new customers to each resource servers
     public boolean newCustomer(int xid, int customerID) throws RemoteException
     {
+      Trace.info("RM::newCustomer(" + xid + ", " + customerID + ") called");
+      Customer customer = (Customer)readData(xid, Customer.getKey(customerID));
+      if (customer == null)
+      {
+        this.getResources().get("roomServer").newCustomer(xid, customerID);
+        this.getResources().get("carServer").newCustomer(xid, customerID);
+        this.getResources().get("flightServer").newCustomer(xid, customerID);
+        //write to server, obv if customer is not in Middleware, it is not anywhere else.
+        customer = new Customer(customerID);
+        writeData(xid, customer.getKey(), customer);
+        Trace.info("RM::newCustomer(" + xid + ", " + customerID + ") created a new customer");
+        return true;
+      }
+      else
+      {
+        Trace.info("INFO: RM::newCustomer(" + xid + ", " + customerID + ") failed--customer already exists");
         return false;
+      }
     }
 
     // TODO: need to ask prof how to delete customers from each resource servers
     public boolean deleteCustomer(int xid, int customerID) throws RemoteException
     {
+      Trace.info("RM::deleteCustomer(" + xid + ", " + customerID + ") called");
+      Customer customer = (Customer)readData(xid, Customer.getKey(customerID));
+      if (customer == null)
+      {
+        Trace.warn("RM::deleteCustomer(" + xid + ", " + customerID + ") failed--customer doesn't exist");
         return false;
+      }
+      else
+      {
+        this.getResources().get("roomServer").deleteCustomer(xid, customerID);
+        this.getResources().get("carServer").deleteCustomer(xid, customerID);
+        this.getResources().get("flightServer").deleteCustomer(xid, customerID);
+        //Call other functions at the server only if customer exists.
+        // Increase the reserved numbers of all reservable items which the customer reserved.
+        RMHashMap reservations = customer.getReservations();
+        for (String reservedKey : reservations.keySet())
+        {
+          ReservedItem reserveditem = customer.getReservedItem(reservedKey);
+          Trace.info("RM::deleteCustomer(" + xid + ", " + customerID + ") has reserved " + reserveditem.getKey() + " " +  reserveditem.getCount() +  " times");
+          ReservableItem item  = (ReservableItem)readData(xid, reserveditem.getKey());
+          Trace.info("RM::deleteCustomer(" + xid + ", " + customerID + ") has reserved " + reserveditem.getKey() + " which is reserved " +  item.getReserved() +  " times and is still available " + item.getCount() + " times");
+          item.setReserved(item.getReserved() - reserveditem.getCount());
+          item.setCount(item.getCount() + reserveditem.getCount());
+          writeData(xid, item.getKey(), item);
+        }
+
+        // Remove the customer from the storage
+        removeData(xid, customer.getKey());
+        Trace.info("RM::deleteCustomer(" + xid + ", " + customerID + ") succeeded");
+        return true;
+      }
     }
 
     // Adds flight reservation to this customer
